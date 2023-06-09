e0fdf175400d107d48e558031d2c92177c4c2c7a
nmargenov <nikolay_margenov@yahoo.com>
2023-06-09 17:41:31 +0300

    get requests to login and register

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,3 +1,10 @@
 const router = require('express').Router();
 
+router.get('/login', (req, res) => {
+    res.status(302).render('users/login');
+});
+
+router.get('/register', (req, res) => {
+    res.status(302).render('users/register');
+});
 module.exports = router;
